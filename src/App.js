2509d442e420d2a1001a7b6aa1364406eadbbb1c
cativerla <69227698+cativerla@users.noreply.github.com>
2020-08-11 21:05:52 +0900

    what?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
 
 function App() {
   return (
-    안녕하세요
+    <div>
+    <p> 안녕하세요 ! </p>
+    <Button Primary> hi </Button>
+    <Progress percent = {33} indicating/>
+    </div>
   );
 }
 
-export default App;
+export default App
